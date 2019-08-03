82f33e8d14709dc7cc6d9a48054184eec67f27e1
IQkairos@gmail.com <IQkairos@gmail.com>
2019-08-03 14:41:11 +0300

    Add project files.

---
+++ Les3Task1Task2/Task1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3Task1Task2
+{
+    public class Task1
+    {
+        public static void DisplayArray(int[] arr) => Console.WriteLine(string.Join(" ", arr));
+        public static void ChangeArray(int[] arr, int direction)
+        {
+            if (direction == 1)
+            {
+                Array.Sort(arr);
+            }
+            else if (direction == 2)
+            {
+                Array.Sort(arr);
+                Array.Reverse(arr);
+            }
+        }
+       public static void CheckSortArray(int[] arr, int direction)
+        {
+            int[] newArray = new int[arr.Length];
+            arr.CopyTo(newArray, 0);
+
+            if (direction == 1)
+            {
+                bool a = false;
+                Array.Sort(newArray);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] != newArray[i])
+                    {
+                        Console.WriteLine("Массив НЕ отсортирован по возратсанию");
+                        a = true;
+                        break;
+                    }
+                }
+                if (a == false)
+                {
+                    Console.WriteLine("Массив отсортирован по возратсанию");
+                }
+
+
+            }
+            if (direction == 2)
+            {
+                bool a = false;
+                Array.Sort(newArray);
+                Array.Reverse(newArray);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] != newArray[i])
+                    {
+                        Console.WriteLine("Массив НЕ отсортирован по убыванию");
+                        a = true;
+                        break;
+                    }
+                }
+                if (a == false)
+                {
+                    Console.WriteLine("Массив отсортирован по убыванию");
+                }
+            }
+        }
+        enum Sort : int { UP = 1, DOWN = 2 };
+        public static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 5, 7, 3, 6 };
+            int x = (int)Sort.UP;
+            int y = (int)Sort.DOWN;
+            ChangeArray(arr1, x);
+            DisplayArray(arr1);
+            Console.WriteLine();
+
+            CheckSortArray(arr1, x);
+        }
+    }
+}
---
+++ Les3Task1Task2/Task2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3Task1Task2
+{
+    class Task2
+    {
+        static void ArifmeticMult(int n, int a1, int t, int mult)
+        {
+            if (n > 0)
+            {
+                int an = a1 + (n - 1) * t;
+                mult *= an;
+                ArifmeticMult(n - 1, a1, t, mult);
+            }
+            else
+            {
+                Console.WriteLine(mult);
+            }
+        }
+        static void geometricMult(int gn, int ga1, int alim, int gmult)
+        {
+            if (alim >= ga1)
+            {
+                Console.WriteLine(gmult);
+            }
+            else
+            {
+                gmult *= ga1;
+
+                geometricMult(gn, ga1 / gn, alim, gmult);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = 5, a1 = 1, t = 1;
+            int mult = 1;
+            ArifmeticMult(n, a1, t, mult);
+            int gn = 2, ga1 = 8, alim = 2, gmult = 1;
+            geometricMult(gn, ga1, alim, gmult);
+        }
+    }
+}
